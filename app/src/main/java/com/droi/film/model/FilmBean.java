bb6cf05cf87c93fe85feb88886db8c164b8c0f34
marekchen <marek@163.com>
2016-12-23 18:04:49 +0800

    upload

--- app/src/main/java/com/droi/film/model/FilmBean.java
+++ app/src/main/java/com/droi/film/model/FilmBean.java
@@ -1,11 +1,35 @@
 package com.droi.film.model;
 
+import com.droi.sdk.core.DroiExpose;
+import com.droi.sdk.core.DroiFile;
+import com.droi.sdk.core.DroiObject;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by marek on 2016/12/20.
  */
 
-public class FilmBean {
-    String name;
-
-
+public class FilmBean extends DroiObject {
+    @DroiExpose
+    String title;        //电影名称
+    @DroiExpose
+    String images;    //封面图片地址
+    @DroiExpose
+    float star;         //评分
+    @DroiExpose
+    int commentsCount;     //评论数量
+    @DroiExpose
+    String summary;       //简介
+    @DroiExpose
+    ArrayList<String> genres;       //类型
+    @DroiExpose
+    ArrayList<String> countries;    //地区
+    @DroiExpose
+    String releaseTime; //上映时间
+    @DroiExpose
+    int durationTime;   //影片时长
 }
